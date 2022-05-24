7530f639ba97d426198d4934a9c204f89d949ead
wellington197 <fcowellcosta@gmail.com>
2022-05-24 14:21:29 -0300

    Update files

---
+++ cypress/integration/ALURAPIC/alurapic.spec.js
@@ -0,0 +1,82 @@
+describe('Login e registro de usuários no alura pic',()=>{
+
+    //primeiro teste a ser executado
+    beforeEach(()=>{
+        //vita o site da Alura
+        cy.visit('https://alura-fotos.herokuapp.com');
+
+    })
+    it('verifica mensagens de validação da tela cadastro',()=>{
+       
+        //verifica se na tag "a" contém o texto Register Now.
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage','Email is required!').should('be.visible');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage','User name is required!').should('be.visible');
+        cy.contains('ap-vmessage','Full name is required!').should('be.visible');
+        cy.contains('ap-vmessage','Password is required!').should('be.visible');
+
+    })
+    
+    it('verifica mensagens de e-mail inválidos',()=>{
+       
+        //verifica se na tag "a" contém o texto Register Now.
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="email"]').type('Wellington');
+        cy.contains('ap-vmessage','Invalid e-mail').should('be.visible');
+        
+        
+    })
+    it('verifica mensagens de senha com menos de 8 caracteres',()=>{
+       
+        //verifica se na tag "a" contém o texto Register Now.
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="password"]').type('123');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage','Mininum length is 8').should('be.visible');       
+        
+    })
+
+    it('verifica se foi digitado o nomes com letra maiúscula',()=>{
+       
+        //verifica se na tag "a" contém o texto Register Now.
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="userName"]').type('WELLINGTON');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage','Must be lower case').should('be.visible');       
+        
+    })
+
+
+
+    //referenciar nosso novo arquivo. e buscar na pasta onde tem o Json
+    const usuarios=require('../../fixtures/usuarios.json')
+    it('Registra novo usuário',()=>{       
+        //verifica se na tag "a" contém o texto Register Now.
+        cy.registra('');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage','Must be lower case').should('be.visible');       
+        
+    })
+
+
+
+    /*Teste do campo Login*/
+    it('Efetuar login com usuário válido',()=>{
+        cy.login('flavio','123');
+        cy.contains('a', '(Logout)').should('be.visible');
+    })  
+
+
+    it('Efetuar login com usuário inválido',()=>{
+        cy.login('wellington','125873');
+        cy.on('window.alert',(str)=>{
+            expert(str).to.equal('invalid user name or password')
+        })  
+
+    })
+})
---
+++ cypress/support/gui_comands.js
@@ -0,0 +1,21 @@
+
+
+/*Criar comando para testes de login*/
+Cypress.Commands.add('login',(nome,senha)=>{
+    cy.get('input[formcontrolname="userName"]').type(nome);
+    cy.get('input[formcontrolname="password"]').type(senha);
+    cy.get('button[type="submit"]').click();
+})
+
+/*Criar comando para testes de login*/
+Cypress.Commands.add('registra',(email,fullName,userName,password)=>{
+    cy.contains('a', 'Register now').click();
+    cy.contains('button', 'Register').click();
+    cy.get('input[formcontrolname="email"]').type(email);
+    cy.get('input[formcontrolname="fullName"]').type(fullName);
+    cy.get('input[formcontrolname="userName"]').type(userName);
+    cy.get('input[formcontrolname="password"]').type(password);
+    cy.contains('button', 'Register').click();
+
+})
+
